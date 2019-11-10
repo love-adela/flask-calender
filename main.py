e76c6440d03c21311d63d62aa116da042f95dcad
love-adelar <love.adelar@gmail.com>
2019-11-10 21:27:18 +0900

    db에 저장된 해쉬값과 비교해서 로그인 가능하도록 함

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask import render_template, request
+import argon2
 
 import db
 
@@ -14,13 +15,19 @@ def main():
 def login():
     username = request.form['username']
     cursor = db.conn.cursor()
-    cursor.execute('select password from account where username=%s;', [username])
+    cursor.execute('select password_hash, password_salt from account where username=%s;', [username])
     result = cursor.fetchone()
     cursor.close()
-    print(username, result[0], request.form['password'])
-    if result[0] == request.form['password']:
+
+    if result is None:
+        return 'Fail'
+
+    db_hash = result[0]
+    salt = result[1]
+    user_hash = argon2.argon2_hash(request.form['password'], salt).hex()
+
+    if db_hash == user_hash:
         return 'Success'
     else:
         return 'Fail'
-    #TODO:PW는 키 파생 함수를 사용해서 단방향 암호화된 값을 저장하자
     return 'testing'
